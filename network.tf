c21efc586f08e521f3ff758f0e6fab3c283898cd
KoLiBer <ckoliber@gmail.com>
2025-07-01 20:20:17 +0330

    fix: change bgp default values

--- network.tf
+++ network.tf
@@ -5,8 +5,8 @@ resource "google_compute_network" "this" {
   description                  = var.description
   routing_mode                 = var.routing_mode
   bgp_best_path_selection_mode = var.path_selection_mode
-  bgp_always_compare_med       = var.path_selection_mode == "STANDARD" ? true : false
-  bgp_inter_region_cost        = var.path_selection_mode == "STANDARD" ? "ADD_COST_TO_MED" : "DEFAULT"
+  bgp_always_compare_med       = var.path_selection_mode == "STANDARD" ? true : null
+  bgp_inter_region_cost        = var.path_selection_mode == "STANDARD" ? "ADD_COST_TO_MED" : null
 
   auto_create_subnetworks                   = false
   delete_default_routes_on_create           = true
